d18b7dec1e51bc0bef646c63d82234c389733505
nduevans <nduevans@gmail.com>
2024-10-12 13:57:36 +0300

    break unit test - testing pipeline

--- test/cdk-github-cicd.test.ts
+++ test/cdk-github-cicd.test.ts
@@ -3,6 +3,6 @@ import { handler } from "../services/hello"
 describe('hello world test group', ()=>{
     test('handler to retun code 200', async ()=>{
         const result = await handler({},{});
-        expect(result.statusCode).toEqual(200);
+        expect(result.statusCode).toEqual(300);
     })
 } )
