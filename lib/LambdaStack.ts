3186225b14637d818fcc932b878406d93c174ef9
nduevans <nduevans@gmail.com>
2024-10-12 13:36:40 +0300

    added lambda function - simple response

--- lib/LambdaStack.ts
+++ lib/LambdaStack.ts
@@ -1,13 +1,26 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { LambdaProps } from "aws-cdk-lib/aws-ses-actions";
 import { Construct } from "constructs";
+import { join } from "path";
 
 interface LambdaStackProps extends StackProps {
-stageName: string
+    stageName: string
 }
 
 export class LambdaStack extends Stack {
     constructor(scope: Construct, id: string, props?: LambdaStackProps){
         super(scope, id, props)
+
+        new NodejsFunction(this, 'Hello-Lambda', {
+            functionName: 'Hello-Lambda',
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'handler',
+            entry: (join(__dirname, '..', 'services', 'hello.ts')),
+            environment: {
+                STAGE: props?.stageName!
+            }
+        })
     } 
 }
